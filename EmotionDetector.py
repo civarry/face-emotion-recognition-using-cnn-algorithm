b83d97de1446d5560573ebf43579fe4937446c0b
civarry <cjcarito15@gmail.com>
2022-12-23 09:39:16 +0800

    final version of system

--- EmotionDetector.py
+++ EmotionDetector.py
@@ -14,12 +14,19 @@ from random import randint
 import tkinter.scrolledtext as st
 import pyautogui
 
+# my_list = []
+# for x in pyautogui.getAllWindows():  
+#     win_name = x.title
+#     if win_name == '' or 'Telegram' in win_name or win_name == 'gameicons' or win_name == 'Program Manager' or win_name == 'Microsoft Text Input Application' or win_name == "ZPToolBarParentWnd" or win_name == "ZPTNativeTransferWnd" or win_name == "Settings" or win_name == "ZPTNativeTransferWnd" or win_name == "Word" or win_name == "ZPTNativeTransferWnd" or win_name == "File Explorer" or win_name == "ZPTNativeTransferWnd" or win_name == "PowerPoint":
+#         continue
+#     my_list.append(str(x.title.title()))
 my_list = []
 for x in pyautogui.getAllWindows():  
-    win_name = (x.title)
-    if win_name == '' or win_name == 'Program Manager' or win_name == 'Microsoft Text Input Application' or win_name == "ZPToolBarParentWnd" or win_name == "ZPTNativeTransferWnd" or win_name == "Settings" or win_name == "ZPTNativeTransferWnd" or win_name == "Word" or win_name == "ZPTNativeTransferWnd" or win_name == "File Explorer" or win_name == "ZPTNativeTransferWnd" or win_name == "PowerPoint":
+    win_name = x.title
+    if win_name == '':
         continue
-    my_list.append(str(x.title.title()))
+    if "Google Chrome" in win_name or "Edge" in win_name or "Opera" in win_name or "Firefox" in win_name or "Browser" in win_name or "Meet" in win_name:
+        my_list.append(str(x.title.title()))
 
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -42,6 +49,7 @@ gamebackground = PhotoImage(file="images/gameicons/gb4.png")
 gbg2 = PhotoImage(file="images/gameicons/game_background.png")
 play_btn = PhotoImage(file="images/gameicons/play.png")
 interval_btn = PhotoImage(file="images/gameicons/interval.png")
+information_btn = PhotoImage(file="images/gameicons/infobtn.png")
 check_btn = PhotoImage(file="images/gameicons/check.png")
 search_btn = PhotoImage(file="images/gameicons/search.png")
 entry_bg = PhotoImage(file="images/gameicons/entrybg.png")
@@ -65,114 +73,132 @@ def only_numbers(char):
     return char.isdigit()
 validation = root.register(only_numbers)
 
-delayervalue = 10
-    
+DELAY_VALUE_MIN = 1
+DELAY_VALUE_MAX = 100
+delayer_value = 10
+
 def delay_btn():
-    global delayervalue
-    interval = intervalEntry.get()
-    delayervalue = int(interval)
-    messagebox.showinfo("Updated Successfully", "You've successfully change the interval value.")
+    interval_str = intervalEntry.get()
+    try:
+        interval = int(interval_str)
+        if interval < DELAY_VALUE_MIN or interval > DELAY_VALUE_MAX:
+            messagebox.showwarning("Value Exceeded", f"Please adjust the value between {DELAY_VALUE_MIN}-{DELAY_VALUE_MAX} only")
+        else:
+            messagebox.showinfo("Updated Successfully", "You've successfully change the interval value.")
+            global delayer_value
+            delayer_value = interval
+    except ValueError:
+        messagebox.showerror("Invalid Value", "Please enter a valid integer value between 1-100.")
+        
+def info_btn():
+    messagebox.showinfo("Notification Frenquency", "You can change the sensitivity of the notification feature here. This allows you to control how often or how quickly you receive notifications when the system detects negative emotions.")
 
 def show(selected_win):
     selected_win = clicked.get()
     def minigame():
-        root.iconify()
-        game_width = 889
-        game_height = 500
-        game_x = (screen_width/2) - (game_width/2)
-        game_y = (screen_height/2) - (game_height/2)
-        gameWindow = Toplevel(root)
-        # sets the title of the
-        gameWindow.title("MiniGame")
+        # Create a Toplevel window for the game
+        game_window_width = 889
+        game_window_height = 500
+        game_window_x = (screen_width / 2) - (game_window_width / 2)
+        game_window_y = (screen_height / 2) - (game_window_height / 2)
+        game_window = Toplevel(root)
+        game_window.title("MiniGame")
+        game_window.geometry('%dx%d+%d+%d' % (game_window_width, game_window_height, game_window_x, game_window_y))
+        game_window.resizable(False, False)
 
-        # sets the geometry of toplevel
-        gameWindow.geometry('%dx%d+%d+%d' % (game_width , game_height, game_x, game_y))
-        gameWindow.resizable('False', 'False')
-        canvas2 = Canvas(gameWindow, width=889,
-                        height=500)
-        canvas2.pack(fill="both", expand=True)
-        canvas2.create_image(0, 0, image=gamebackground,
-                            anchor="nw")
+        def shuffle_word():
+            # Shuffle the word entered by the user
+            original_word = my_entry_box.get().lower()
+            global shuffled_word
+            global original_word_global
+            shuffled_word = original_word
+            original_word_global = original_word
+            shuffled_word = shuffle(shuffled_word)
+            my_tk_label['text'] = shuffled_word
 
-        def shuffle(s):
-            stringLenght = len(s)
-            li = list(s)
+        def shuffle(word):
+            # Shuffle the characters in the given word
+            word_length = len(word)
+            characters = list(word)
 
-            for i in range(0, stringLenght - 1):
-                pos = randint(i + 1, stringLenght - 1)
+            for i in range(0, word_length - 1):
+                pos = randint(i + 1, word_length - 1)
 
-                li[pos], li[i] = li[i], li[pos]
-                res = ""
-                for i in range(stringLenght):
-                    res = res + li[i]
-                return res
+                characters[pos], characters[i] = characters[i], characters[pos]
+            shuffled_word = ""
+            for i in range(word_length):
+                shuffled_word = shuffled_word + characters[i]
+            return shuffled_word
 
-        def Randomized():
-            getresult = MyEntryBox.get().lower()
-            global s
-            global x
-            s = getresult
-            x = s
-            shuffle(s)
-            myTKlabel['text'] = shuffle(s)
-
-        def CheckInputValue():
-            answer = answerEntry.get().lower()
+        def check_input_value():
+            # Check if the user's answer is correct
+            answer = answer_entry.get().lower()
             if answer == '':
-                messagebox.showwarning("Word not found", "Please try again!", parent=gameWindow)
-            elif answer == x:
-                messagebox.showinfo("Congratulation", "You are Correct!", parent=gameWindow)
+                messagebox.showwarning("Word not found", "Please try again!", parent=game_window)
+            elif answer == original_word_global:
+                messagebox.showinfo("Congratulations", "You are Correct!", parent=game_window)
             else:
-                messagebox.showwarning("Wrong", "Want to try again?", parent=gameWindow)
-        meaning_width = 550
-        meaning_height = 280        
-        meaning_x = (screen_width/2) - (meaning_width/2)
-        meaning_y = (screen_height/2) - (meaning_height/2)
-        def meaning():            
-            dictionary=PyDictionary()
-            meanings_list = dictionary.meaning(s)
+                messagebox.showwarning("Wrong", "Want to try again?", parent=game_window)
+
+        def show_meaning():
+            # Show the meaning of the original word
+            dictionary = PyDictionary()
+            meanings_list = dictionary.meaning(original_word_global)
             strip_word = str(meanings_list).replace("{","").replace("}", "").replace("[", '').replace("]", '').replace("'", '')
             if strip_word == 'None':
-                messagebox.showwarning("Definition not found", "Please check the spelling", parent=gameWindow)
-            elif s == '':
-                messagebox.showwarning("Word not found", "Please try again!", parent=gameWindow)
+                messagebox.showwarning("Definition not found", "Please check the spelling", parent=game_window)
+            elif original_word_global == '':
+                messagebox.showwarning("Word not found", "Please try again!", parent=game_window)
             else:
+                # Create a new window to display the meaning
+                meaning_window_width = 550
+                meaning_window_height = 280        
+                meaning_window_x = (screen_width / 2) - (meaning_window_width / 2)
+                meaning_window_y = (screen_height / 2) - (meaning_window_height / 2)
                 secondary_window = Toplevel()
-                secondary_window.title(f'The meaning of "{x.upper()}"')
-                secondary_window.geometry('%dx%d+%d+%d' % (meaning_width , meaning_height, meaning_x, meaning_y))
+                secondary_window.title(f'The meaning of "{original_word_global.upper()}"')
+                secondary_window.geometry('%dx%d+%d+%d' % (meaning_window_width, meaning_window_height, meaning_window_x, meaning_window_y))
                 secondary_window.resizable(False, False)
-                canvas_board = Canvas(secondary_window, width=600,
-                            height=350)
+                
+                # Create a canvas to hold the meaning text
+                canvas_board = Canvas(secondary_window, width=600, height=350)
                 canvas_board.configure(bg='#ffffff')
                 canvas_board.pack(fill="both", expand=True)
-                text_area = st.ScrolledText(canvas_board,width = 30,fg='#582308', bg="#f5b98c", height = 8, font = ("Bookman Old Style",20))
-                text_area.grid(column = 0, pady = 10, padx = 10)
-                # Inserting Text which is read only
-                text_area.insert(INSERT,strip_word)
-                # Making the text read only
-                text_area.configure(state ='disabled')
+                
+                # Create a ScrolledText widget to display the meaning
+                text_area = st.ScrolledText(canvas_board, width=30, fg='#582308', bg="#f5b98c", height=8, font=("Bookman Old Style", 20))
+                text_area.grid(column=0, pady=10, padx=10)
+                
+                # Insert the meaning text into the widget and make it read-only
+                text_area.insert(INSERT, strip_word)
+                text_area.configure(state='disabled')
 
-
-        # Create Tkinter Entry Widget
-        MyEntryBox = Entry(canvas2, borderwidth=0, show="•",
-                        width=14,fg='#d6bda9', bg="#825e3e",font=('Bookman Old Style', 38))
-        myTKlabel = Label(canvas2, borderwidth=0, relief="ridge",
+        # Create the game window when the main window is minimized
+        # Create a canvas to hold the game elements
+        game_canvas = Canvas(game_window, width=889, height=500)
+        game_canvas.pack(fill="both", expand=True)
+        game_canvas.create_image(0, 0, image=gamebackground, anchor="nw")
+        
+        # Create a Tkinter Entry widget for the user to input a word
+        my_entry_box = Entry(game_canvas, borderwidth=0, show="•", width=14,fg='#d6bda9', bg="#825e3e",font=('Bookman Old Style', 38))
+        my_tk_label = Label(game_canvas, borderwidth=0, relief="ridge",
                         width=15,fg='#d6bda9',bg="#825e3e", font=('Bookman Old Style', 37))
-        answerEntry = Entry(canvas2, borderwidth=0,bg="#825e3e", width=14,fg='#d6bda9',
+        answer_entry = Entry(game_canvas, borderwidth=0,bg="#825e3e", width=14,fg='#d6bda9',
                             font=('Bookman Old Style', 38))
-        randomizedButton = Button(
-            canvas2, borderwidth=0, image=play_btn, bg='#825e3e', height=61, width=61, command=Randomized)
-        answerChckButton = Button(
-            canvas2, borderwidth=0, image=check_btn,bg='#825e3e', height=61, width=61, command=CheckInputValue)
-        searchButton = Button(canvas2, borderwidth=0,image=search_btn,bg='#825e3e', height=61, width=61, command=meaning)
-
-        MyEntryBox.place(x=214, y=62)
-        myTKlabel.place(x=215, y=184)
-        answerEntry.place(x=214, y=312)
-        randomizedButton.place(x=622, y=62)
-        answerChckButton.place(x=622, y=312)
-        searchButton.place(x=560, y=312)
-        # searchButton.place(x=(game_width/2) - (73/2), y=312)
+        
+        # Create buttons for the user to shuffle the word and check their answer
+        shuffle_button = Button(game_canvas, borderwidth=0, image=play_btn, bg='#825e3e', height=61, width=61, command=shuffle_word)
+        check_button = Button(game_canvas, borderwidth=0, image=check_btn,bg='#825e3e', height=61, width=61, command=check_input_value)
+        meaning_button = Button(game_canvas, borderwidth=0,image=search_btn,bg='#825e3e', height=61, width=61, command=show_meaning)
+        
+        # Place the widgets on the canvas
+        my_entry_box.place(x=214, y=62)
+        my_tk_label.place(x=215, y=184)
+        answer_entry.place(x=214, y=312)
+        shuffle_button.place(x=622, y=62)
+        check_button.place(x=622, y=312)
+        meaning_button.place(x=560, y=312)
+        root.iconify()
         ################################################# end of game #################################################
 
 
@@ -192,7 +218,7 @@ def show(selected_win):
 
     # load weights into new model
     emotion_model.load_weights("emotion_model(75).h5")
-    print("Loaded model from disk")
+    # print("Loaded model from disk")
 
     # start the webcam feed
     #cap = cv2.VideoCapture(0)
@@ -227,8 +253,6 @@ def show(selected_win):
                 # cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
                 roi_gray_frame = gray_frame[y:y + h, x:x + w]
                 cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-                # count number of faces
-                countFaces = len(num_faces)
                 # predict the emotions
                 emotion_prediction = emotion_model.predict(cropped_img)
                 maxindex = int(np.argmax(emotion_prediction))
@@ -274,7 +298,7 @@ def show(selected_win):
 
                 percentageOfEmotion = numberOfHappyFace / countFaces * 100
                 percentageOfNegativeEmtn = numberOfNotSmiling / countFaces * 100
-                frameX, frameY, frameWidth, FrameHeight = 0, 0, 250, 100
+                frameX, frameY, frameWidth, FrameHeight = 0, 0, 220, 30
 
                 if maxindex == 0:
                     x1, y1 = x+w, y+h
@@ -299,18 +323,23 @@ def show(selected_win):
                     x1, y1 = x+w, y+h
                     bc_6.emotion_rect()
 
-                cv2.rectangle(frame, (frameX, frameX), (frameX + frameWidth, frameY + FrameHeight), (0, 0, 0), -1)
+                cv2.rectangle(frame, (10, 10), (10 + frameWidth, 10 + FrameHeight), (97,107,255), -1)
+                cv2.circle(frame, (224, 25), 16, (97,107,255), -2)
+                cv2.rectangle(frame, (10, 42), (10 + frameWidth, 42 + FrameHeight), (144,238,144), -1)
+                cv2.circle(frame, (224, 57), 16, (144,238,144), -2)
+                cv2.rectangle(frame, (10, 74), (10 + frameWidth, 74 + FrameHeight), (230,211,139), -1)
+                cv2.circle(frame, (224, 89), 16, (230,211,139), -2)
                 # Show the data of the emotion here
-                cv2.putText(frame, "Number of Students: " + str(countFaces), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0, 1), 1)
-                cv2.putText(frame, "Number of Smiling: " + str(numberOfHappyFace), (10, 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0, 1), 1)
-                cv2.putText(frame, "Percentage of Happy: " + f'{percentageOfEmotion:0.1f}', (10, 80),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0, 1), 1)
+                cv2.putText(frame, "Number of Students: " + str(countFaces), (12, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                cv2.putText(frame, "Number of Smiling: " + str(numberOfHappyFace), (12, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                cv2.putText(frame, "Positive Emotion: " + f'{percentageOfEmotion:0.1f}%', (12, 95),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
             if (initialize == True and percentageOfNegativeEmtn == 100) and percentageOfEmotion == 0:
                 noExistenceOfEmtn += 1
                 # counting += 1
                 # print(delayervalue)
-                if noExistenceOfEmtn == delayervalue:
+                if noExistenceOfEmtn == delayer_value:
                     MsgBox = messagebox.askquestion(
                         'Notification', 'Do you want to open the minigame', icon='warning')
                     if MsgBox == 'yes':
@@ -321,12 +350,13 @@ def show(selected_win):
                         break
                     else:
                         noExistenceOfEmtn = 0
+                        root.iconify()
                         messagebox.showinfo(
                             'Return', 'You will now return to the application screen')
             elif initialize == True and percentageOfEmotion <= PERCENTAGEOFPSTEMTN:
                 existenceOfEmtn += 1
                 # print(delayervalue)
-                if existenceOfEmtn == delayervalue:
+                if existenceOfEmtn == delayer_value:
                     MsgBox = messagebox.askquestion(
                         'Notification', 'Do you want to open the minigame', icon='warning')
                     if MsgBox == 'yes':
@@ -375,6 +405,8 @@ intervalEntry.insert(0, "(1-100)")
 intervalEntry.place(x=202, y=276)
 intervalButton = Button(canvas1, borderwidth=0, image=interval_btn,  command=delay_btn)
 intervalButton.place(x=368, y=275)
+infoButton = Button(canvas1, borderwidth=0, image=information_btn,  command=info_btn)
+infoButton.place(x=380, y=251)
 drop = OptionMenu( canvas1 , clicked , *options, command=show)
 drop.place(x=202, y=200)
 drop.config(width = 15, font=('Century Gothic', 15, 'bold'))
